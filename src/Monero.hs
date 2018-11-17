2cfeb65378c17413cfe659f000deef9e4f78a71c
Ian Shipman <ics@gambolingpangolin.com>
2018-11-17 14:34:29 -0600

    adds some address types

---
+++ src/Monero.hs
@@ -0,0 +1,41 @@
+-- |
+-- Module    : Monero
+-- Stability : experimental
+
+module Monero () where
+
+import           Crypto.ECC.Ed25519
+
+
+-- | Every Monero output is owned by a keypair consisting of two Ed25519 keys.
+-- These keys are used differently from one another and have different
+-- consequences if compromised (separately).
+data PublicAddress
+  = PublicAddress
+  { spendPublic :: Point
+  , viewPublic  :: Point
+  } deriving Eq
+
+
+-- | This is the pair of private keys associated with a 'PublicAddress'
+data SpendKey
+  = SpendKey
+  { spendPriv :: Scalar
+  , viewPriv  :: Scalar
+  } deriving Eq
+
+
+-- | All Monero payments spend to ephemeral addresses, which only the owner of
+-- the view private key can recognize.
+--
+-- - [ ] Serialize instance
+newtype StealthAddress = StealthAddress { unStealthAddress :: Point }
+  deriving Eq
+
+
+-- | The view (private) key of an address can be shared with a third party to
+-- give them visibility over the transactions bound for the address, without
+-- giving them the ability to spend
+newtype ViewKey = ViewKey { unViewKey :: Scalar }
+  deriving Eq
+
