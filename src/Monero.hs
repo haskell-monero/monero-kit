8c55ad6e5037695b4ebba6ac0080b1286dbcae07
Ian Shipman <ics@gambolingpangolin.com>
2018-12-04 19:00:40 -0600

    draft block datatype

--- src/Monero.hs
+++ src/Monero.hs
@@ -2,10 +2,18 @@
 -- Module    : Monero
 -- Stability : experimental
 
+{-# LANGUAGE RecordWildCards #-}
+
 module Monero () where
 
-import           Crypto.ECC.Ed25519
+import           Crypto.ECC.Edwards25519
+import           Crypto.Hash
+import           Data.Serialize
+import           Data.Word
 
+-- ~~~~~~~~~ --
+-- Addresses --
+-- ~~~~~~~~~ --
 
 -- | Every Monero output is owned by a keypair consisting of two Ed25519 keys.
 -- These keys are used differently from one another and have different
@@ -39,3 +47,66 @@ newtype StealthAddress = StealthAddress { unStealthAddress :: Point }
 newtype ViewKey = ViewKey { unViewKey :: Scalar }
   deriving Eq
 
+
+-- ~~~~~~~~~~~~ --
+-- Transactions --
+-- ~~~~~~~~~~~~ --
+
+
+data Transaction = Transaction
+
+-- ~~~~~~ --
+-- Blocks --
+-- ~~~~~~ --
+
+-- | A Monero block header
+data BlockHeader
+  = BlockHeader
+  { majorVersion   :: Word8
+  , minorVersion   :: Word8
+  , blockTimestamp :: Word64
+  , previousBlock  :: _
+  -- ^ Keccak with 1600-bit digest
+  , blockNonce     :: Word32
+  } deriving Eq
+
+instance Serialize BlockHeader where
+
+  put BlockHeader{..} =
+    BlockHeader
+      <$> putVarint
+      <*> putVarint
+      <*> _ -- putWord64be ?
+      <*> _
+      <*> _
+
+  get =
+    BlockHeader
+    <$> getVarint
+    <*> getVarint
+    <*> _
+    <*> _
+    <*> _
+
+
+-- | A Monero block
+data Block
+  = Block
+  { blockHeader       :: BlockHeader
+  , coinbaseTx        :: Transaction
+  , transactionHashes :: _
+  -- ^ what structure do these hashes have ?
+  } deriving Eq
+
+
+-- ~~~~~~~~~~~~~~~~~~~~~ --
+-- Serialization helpers --
+-- ~~~~~~~~~~~~~~~~~~~~~ --
+
+data Varint = Varint
+
+getVarint :: Get Varint
+getVarint = _
+
+putVarint :: Putter Varint
+putVarint = _
