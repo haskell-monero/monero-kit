4bf73ad1f5ff355173b9bb7a15f4d48a4c39b460
Matthew Wraith <wraithm@gmail.com>
2018-12-08 14:53:33 -0600

    Adding transaction types

--- src/Monero.hs
+++ src/Monero.hs
@@ -9,33 +9,35 @@ import           Crypto.Error
 import           Crypto.Hash
 import           Data.ByteArray
 import           Data.ByteString
+import           GHC.Word
 
 
 -- | Every Monero output is owned by a keypair consisting of two Ed25519 keys.
 -- These keys are used differently from one another and have different
 -- consequences if compromised (separately).
 data PublicAddress
-  = PublicAddress
-  { spendPublic :: Point
-  , viewPublic  :: Point
-  } deriving Eq
+    = PublicAddress
+    { spendPublic :: Point
+    , viewPublic  :: Point
+    } deriving Eq
 
 
 -- | This is the pair of private keys associated with a 'PublicAddress'
 data SpendKey
-  = SpendKey
-  { spendPriv :: Scalar
-  , viewPriv  :: Scalar -- Usually a hash of the 'spendPriv'
-  } deriving Eq
+    = SpendKey
+    { spendPriv :: Scalar
+    , viewPriv  :: Scalar -- Usually a hash of the 'spendPriv'
+    } deriving Eq
 
 
--- | Generate a 'SpendKey' from the spend part of the 'SpendKey' by taking a hash of the private key for the view key.
+-- | Generate a 'SpendKey' from the spend part of the 'SpendKey' by taking a
+-- hash of the private key for the view key.
 spendKey :: Scalar -> SpendKey
-spendKey pk = SpendKey pk (keccakScalar pk)
+spendKey pk = SpendKey pk (keccak256Scalar pk)
 
 
-keccakScalar :: Scalar -> Scalar
-keccakScalar = throwCryptoError . scalarDecodeLong . scrubbedKeccak . scalarEncode
+keccak256Scalar :: Scalar -> Scalar
+keccak256Scalar = throwCryptoError . scalarDecodeLong . scrubbedKeccak . scalarEncode
 
 
 scrubbedKeccak :: ByteString -> ScrubbedBytes
@@ -51,11 +53,43 @@ keccak256 = hash
 --
 -- - [ ] Serialize instance
 newtype StealthAddress = StealthAddress { unStealthAddress :: Point }
-  deriving Eq
+    deriving Eq
 
 
 -- | The view (private) key of an address can be shared with a third party to
 -- give them visibility over the transactions bound for the address, without
 -- giving them the ability to spend
 newtype ViewKey = ViewKey { unViewKey :: Scalar }
-  deriving Eq
+    deriving Eq
+
+
+data Transaction
+    = Transaction
+    { version       :: Word64
+    , unlockTime    :: Word64
+    , vin           :: [TxIn]
+    , vout          :: [TxOut]
+    , extra         :: ByteString
+    , signatures    :: [[Signature]]
+    , rctSignatures :: RctSig
+    } deriving Eq
+
+
+data Signature = Signature
+    deriving Eq
+
+
+data RctSig = RctSig
+    deriving Eq
+
+
+data TxIn
+    = TxIn
+    {
+    } deriving Eq
+
+
+data TxOut
+    = TxOut
+    {
+    } deriving Eq
